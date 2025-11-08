8aa362627423aba2ccfe2a25841a651180a40cc2
Rohan Kumar Raj <rohan0902raj@gmail.com>
2025-11-08 23:04:32 +0530

    solved two sum

---
+++ Step_3_Solve_Problem_On_Array/Lec_2_Medium/twoSum.cpp
@@ -0,0 +1,29 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> myMap;
+        vector<int> ans= {-1,-1};
+        for(int i=0;i<nums.size();i++){
+            myMap[nums[i]] = i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int comp = target - nums[i];
+            if(myMap.find(comp)!=myMap.end() && myMap[comp]!=i){
+                ans[0]=i;
+                ans[1]=myMap[comp];
+                break;
+            }
+        }
+        return ans;
+    }
+};
